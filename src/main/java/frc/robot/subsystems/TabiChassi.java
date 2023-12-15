755e27ac5831ec92ec9becf98eec10074d99cee9
No-aa <136657912+No-aa@users.noreply.github.com>
2023-12-14 20:33:17 -0800

    Update TabiChassi.java

--- src/main/java/frc/robot/subsystems/TabiChassi.java
+++ src/main/java/frc/robot/subsystems/TabiChassi.java
@@ -9,34 +9,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TabiChassi extends SubsystemBase {
-
-    public CANSparkMax leftCanSparkMax = new CANSparkMax(5, MotorType.kBrushless);
-    public CANSparkMax rightCanSparkMax = new CANSparkMax(8, MotorType.kBrushless);
-    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(6, MotorType.kBrushless);
-    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(1, MotorType.kBrushless);
+    public CANSparkMax leftCanSparkMax = new CANSparkMax(1, MotorType.kBrushless);
+    public CANSparkMax rightCanSparkMax = new CANSparkMax(3, MotorType.kBrushless);
+    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(2, MotorType.kBrushless);
+    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(4, MotorType.kBrushless);
     public DifferentialDrive drivetrain = new DifferentialDrive(leftCanSparkMax, rightCanSparkMax);
-    public RelativeEncoder leftEncoder = leftCanSparkMax.getEncoder();
-    public RelativeEncoder rightEncoder = rightCanSparkMax.getEncoder();
-    public RelativeEncoder leftFollowerEncoder = leftFollowerCanSparkMax.getEncoder();
-    public RelativeEncoder rightFollowerEncoder = rightFollowerCanSparkMax.getEncoder();
-
-    public TabiChassi() {
-        for (CANSparkMax m : new CANSparkMax[] { leftCanSparkMax, rightCanSparkMax, leftFollowerCanSparkMax,rightFollowerCanSparkMax }) {
-            m.restoreFactoryDefaults(false);
+    public RelativeEncoder m_leftEncoder = leftCanSparkMax.getEncoder();
+    public RelativeEncoder m_rightEncoder = rightCanSparkMax.getEncoder();
+    public TabiChassi(){
+        m_leftEncoder.setPosition(0);
+        m_rightEncoder.setPosition(0);
+        for(CANSparkMax m : new CANSparkMax []{leftCanSparkMax, rightCanSparkMax,leftFollowerCanSparkMax,rightFollowerCanSparkMax}){
+            m.setIdleMode(IdleMode.kBrake);
+            m.setSmartCurrentLimit(240/4, 240/4);//240 is sensible current limit to chassis
             m.clearFaults();
-
-            m.setIdleMode(IdleMode.kCoast);
-            // m.setIdleMode(IdleMode.kBrake);
-
-            m.setSmartCurrentLimit(240 / 4, 240 / 4);// 240 is sensible current limit to chassis
         }
         leftEncoder.setPosition(0);
         rightEncoder.setPosition(0);
 
         leftCanSparkMax.setInverted(false);
         rightCanSparkMax.setInverted(true);
-        leftFollowerCanSparkMax.setInverted(false);
-        rightFollowerCanSparkMax.setInverted(false);
+        leftFollowerCanSparkMax.setIdleMode(IdleMode.kCoast);
+        rightFollowerCanSparkMax.setIdleMode(IdleMode.kCoast);    
 
         // leftEncoder.setPositionConversionFactor(TabiChassi.EncoderDistancePerPulse);
         // leftEncoder.setVelocityConversionFactor(TabiChassi.EncoderDistancePerPulse /
@@ -45,14 +39,14 @@ public class TabiChassi extends SubsystemBase {
         // rightEncoder.setVelocityConversionFactor(TabiChassi.EncoderDistancePerPulse /
         // 60.0); // RPM to m/s
 
-        // configure followers
-        // leftFollowerCanSparkMax.follow(leftCanSparkMax);
-        // rightFollowerCanSparkMax.follow(rightCanSparkMax);
+
+        leftFollowerCanSparkMax.follow(leftCanSparkMax);
+        rightFollowerCanSparkMax.follow(rightCanSparkMax);
 
     }
 
     public void arcadeDrive(double power, double turn) {
-        drivetrain.arcadeDrive(power, turn);
+    drivetrain.arcadeDrive(power / 2  ,turn / 2 * -1);
     }
 
     public void periodic() {
