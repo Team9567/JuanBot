ae7bbe93345c3506ce94c1bce696e2e7000b9c8a
Noa Oei <noa.oei9@gmail.com>
2023-12-14 20:24:43 -0800

    fixed movement

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
