d0d3b5846a0af294e1460c414d00903239e9b55a
DriverStation <tekdemo+driverstation@gmail.com>
2024-01-02 19:53:35 -0800

    Added all of the stuff for smart dashboard. Added a limit on the motors and an inversion on the turn.

--- src/main/java/frc/robot/subsystems/TabiChassi.java
+++ src/main/java/frc/robot/subsystems/TabiChassi.java
@@ -7,13 +7,15 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 public class TabiChassi extends SubsystemBase {
-    public CANSparkMax leftCanSparkMax = new CANSparkMax(5, MotorType.kBrushless);
-    public CANSparkMax rightCanSparkMax = new CANSparkMax(8, MotorType.kBrushless);
-    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(6, MotorType.kBrushless);
-    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(1, MotorType.kBrushless);
+    public CANSparkMax leftCanSparkMax = new CANSparkMax(1, MotorType.kBrushless);
+    public CANSparkMax rightCanSparkMax = new CANSparkMax(3, MotorType.kBrushless);
+    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(2, MotorType.kBrushless);
+    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(4, MotorType.kBrushless);
     public DifferentialDrive drivetrain = new DifferentialDrive(leftCanSparkMax, rightCanSparkMax);
+   //Gets the encoder distances
     public RelativeEncoder leftEncoder = leftCanSparkMax.getEncoder();
     public RelativeEncoder rightEncoder = rightCanSparkMax.getEncoder();
     public RelativeEncoder leftFollowerEncoder = leftFollowerCanSparkMax.getEncoder();
@@ -24,18 +26,16 @@ public class TabiChassi extends SubsystemBase {
             m.restoreFactoryDefaults(false);
             m.clearFaults();
 
-            m.setIdleMode(IdleMode.kCoast);
-            // m.setIdleMode(IdleMode.kBrake);
+            //m.setIdleMode(IdleMode.kCoast);  // NO BRAKES!!!!!!
+            m.setIdleMode(IdleMode.kBrake);  // BRAKES!!!
 
-            m.setSmartCurrentLimit(240 / 4, 240 / 4);// 240 is sensible current limit to chassis
+            m.setSmartCurrentLimit(240 / 4, 240 / 4);// 240 is sensible current limit to chassis.  12-7-23 NateO - Noticed /4 to limit current by 1/4 and slow robot down.
         }
         leftEncoder.setPosition(0);
         rightEncoder.setPosition(0);
 
         leftCanSparkMax.setInverted(false);
         rightCanSparkMax.setInverted(true);
-        leftFollowerCanSparkMax.setInverted(false);
-        rightFollowerCanSparkMax.setInverted(false);
 
         // leftEncoder.setPositionConversionFactor(TabiChassi.EncoderDistancePerPulse);
         // leftEncoder.setVelocityConversionFactor(TabiChassi.EncoderDistancePerPulse /
@@ -45,33 +45,44 @@ public class TabiChassi extends SubsystemBase {
         // 60.0); // RPM to m/s
 
         // configure followers
-        //leftFollowerCanSparkMax.follow(leftCanSparkMax);
-        //rightFollowerCanSparkMax.follow(rightCanSparkMax);
+        leftFollowerCanSparkMax.follow(leftCanSparkMax);
+        rightFollowerCanSparkMax.follow(rightCanSparkMax);
 
     }
+    public double getEncoderSpeed(){
+        var leftSpeed = leftEncoder.getVelocity();
+        var rightSpeed = rightEncoder.getVelocity();
+        var averageSpeed = (rightSpeed + leftSpeed)/2;
+        return averageSpeed;
+    }
 
+    //Takes the encoder positions and divides them by 2
     public double getEncoderDistance(){
-        var left = leftCanSparkMax.getEncoder().getPosition();
-        var right = rightCanSparkMax.getEncoder().getPosition();
-        var average = (right + left)/2;
-        return average;        
+        var left = leftCanSparkMax.getEncoder().getPosition(); // the left drive motor position
+        var right = rightCanSparkMax.getEncoder().getPosition(); // the right drive motor position
+        var average = (right + left)/2;  // our average position of both left and right drive motors combined: sum and divide by 2. 
+        return average;  
     }
 
     public void arcadeDrive(double power, double turn) {
-        drivetrain.arcadeDrive(power, turn);
+        drivetrain.arcadeDrive(power * power, turn * -1);
         //drivetrain.arcadeDrive(power / 4, turn / 4);
     }
 
     public void periodic() {
-        SmartDashboard.putNumber("Chasis/leftLeaderCurrent", leftCanSparkMax.getOutputCurrent());
-        SmartDashboard.putNumber("Chasis/rightLeaderCurrent", rightCanSparkMax.getOutputCurrent());
-        SmartDashboard.putNumber("Chasis/leftFollowerCurrent", leftFollowerCanSparkMax.getOutputCurrent());
-        SmartDashboard.putNumber("Chasis/rightFollowerCurrent", rightFollowerCanSparkMax.getOutputCurrent());
-
-        SmartDashboard.putNumber("Chasis/leftLeader", leftEncoder.getPosition());
-        SmartDashboard.putNumber("Chasis/rightLeader", rightEncoder.getPosition());
-        SmartDashboard.putNumber("Chasis/leftFollower", leftFollowerEncoder.getPosition());
-        SmartDashboard.putNumber("Chasis/rightFollower", rightFollowerEncoder.getPosition());
+        // Average position from drive motors from the get encoder distance command
+        SmartDashboard.putNumber("AverageDistance", getEncoderDistance());
+        SmartDashboard.putNumber("AverageSpeed", getEncoderSpeed());
+        // Currents from drive motors
+        SmartDashboard.putNumber("Chassis/leftLeaderCurrent", leftCanSparkMax.getOutputCurrent());
+        SmartDashboard.putNumber("Chassis/rightLeaderCurrent", rightCanSparkMax.getOutputCurrent());
+        SmartDashboard.putNumber("Chassis/leftFollowerCurrent", leftFollowerCanSparkMax.getOutputCurrent());
+        SmartDashboard.putNumber("Chassis/rightFollowerCurrent", rightFollowerCanSparkMax.getOutputCurrent());
+        // Positions from drive motors
+        SmartDashboard.putNumber("Chassis/leftLeader", leftEncoder.getPosition());
+        SmartDashboard.putNumber("Chassis/rightLeader", rightEncoder.getPosition());
+        SmartDashboard.putNumber("Chassis/leftFollower", leftFollowerEncoder.getPosition());
+        SmartDashboard.putNumber("Chassis/rightFollower", rightFollowerEncoder.getPosition());
     }
 }
      
